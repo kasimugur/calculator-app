b5dad9d86a1e7fd83fdea4dad1e96091131169c6
kasimugur <kasiugur7@gmail.com>
2024-08-26 20:24:44 +0300

    scss added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,30 +1,66 @@
+import { useContext } from "react"
+import { CalcContext } from "../context"
 
 export default function Button() {
+  const { num, setNum, calculator, setScore, setSign } = useContext(CalcContext)
+  console.log("nummm", num)
   return (
     <>
-    <div className="card">
-    <button className="btn">7</button>
-    <button className="btn">8</button>
-    <button className="btn">9</button>
-    <button className="btn">DEL</button>
-    <button className="btn">4</button>
-    <button className="btn">5</button>
-    <button className="btn">6</button>
-    <button className="btn">+</button>
-    <button className="btn">1</button>
-    <button className="btn">2</button>
-    <button className="btn">3</button>
-    <button className="btn">-</button>
-    <button className="btn">.</button>
-    <button className="btn">0</button>
-    <button className="btn">/</button>
-    <button className="btn">x</button>
-    <div className="btnReset">
-    <button className="btn">RESET</button>
-    <button className="btn">=</button>
+      <div className="card">
+        <button
+          value={7} onClick={(e) => setNum(e.target.value)}
 
-    </div>
-    </div>
+          className="btn">7</button>
+        <button
+          value={8} onClick={(e) => setNum(e.target.value)} className="btn"
+        >8</button>
+        <button
+          value={9} onClick={(e) => setNum(e.target.value)} className="btn"
+        >9</button>
+        <button
+          onClick={(e) => setNum(e.target.value)} className="btn"
+        >DEL</button>
+        <button
+          value={4} onClick={(e) => setNum(e.target.value)} className="btn"
+        >4</button>
+        <button
+          value={5} onClick={(e) => setNum(e.target.value)} className="btn"
+        >5</button>
+        <button
+          value={6} onClick={(e) => setNum(e.target.value)} className="btn"
+        >6</button>
+        <button
+          value={'+'} onClick={(e) => setSign(e.target.value)} className="btn"
+        >+</button>
+        <button
+          value={1} onClick={(e) => setNum(e.target.value)} className="btn"
+        >1</button>
+        <button
+          value={2} onClick={(e) => setNum(e.target.value)} className="btn"
+        >2</button>
+        <button
+          value={3} onClick={(e) => setNum(e.target.value)} className="btn"
+        >3</button>
+        <button
+          value={"-"} onClick={(e) => setSign(e.target.value)} className="btn"
+        >-</button>
+        <button
+          value={","} onClick={(e) => setSign(e.target.value)} className="btn"
+        >.</button>
+        <button
+          value={0} onClick={(e) => setNum(e.target.value)} className="btn"
+        >0</button>
+        <button
+          value={"/"} onClick={(e) => setSign(e.target.value)} className="btn"
+        >/</button>
+        <button
+          value={"*"} onClick={(e) => setSign(e.target.value)} className="btn"
+        >x</button>
+        <div className="btnReset">
+          <button onClick={() => setScore('')} className="btn">RESET</button>
+          <button onClick={() => calculator()} className="btn">=</button>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,21 +5,53 @@ import Button from "./Button";
 export default function Calculator() {
   const [num, setNum] = useState('')
   const [score, setScore] = useState('')
+  const [sign, setSign] = useState("")
+
+  const calculator = () => {
+    // const scores = Number(num)
+    // const scores2 = Number(num)
+    if (sign === "*") {
+      const basket = + num
+      console.log("baskettt", basket)
+    }
+    // if (sign === "+") {
+    //   const total = scores + scores2
+    //   setScore(total)
+    // }
+    // if (sign === "-") {
+    //   const total = scores - scores2
+    //   setScore(total)
+    // }
+    // if (sign === "*") {
+    //   const total = scores * scores2
+    //   setScore(total)
+    // }
+    // if (sign === "/") {
+    //   const total = scores / scores2
+    //   setScore(total)
+    // }
+  }
+  const data = { num, setNum, calculator, setScore, setSign }
+  console.log("scoreeee--", score)
+  console.log("signnn--", sign)
   return (
     <>
-      <CalcContext.Provider>
+      <CalcContext.Provider value={data} >
         <div className="header">
           <h1>calc</h1>
-          <label>THEME
-            <input type="radio" id="inp-rad" />
-          </label>
+          <div>
+            theme
+            <div className="radio">
+
+              <span>1 2 3</span>
+              <input type="radio" id="inp-rad" />
+            </div>
+          </div>
         </div>
-        <form className="main">
-          <input
-          onChange={e => setNum(e.target.value)}
-          type="number" id="inp-num" />
-          <h1>{num} </h1>
-        </form>
+        <div
+          className="main"
+          value={score}
+          type="number">399,981</div>
         <Button />
       </CalcContext.Provider>
     </>
